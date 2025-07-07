5225bf2735a3265057e8066df120121031abda19
Andrew Newman <andrewfnewman@gmail.com>
2025-07-07 14:04:44 +1000

    Finish setting data directory to top level.

--- process.sh
+++ process.sh
@@ -2,6 +2,12 @@
 
 cd notebooks
 
+export PYTHONHASHSEED=0
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+
 for item in "."/*; do
     # Check if the current item is a regular file
     if [[ -f "$item" ]]; then
