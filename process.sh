147c905ce8bb1151b78a451101e9024d3ddcb795
Andrew Newman <andrewfnewman@gmail.com>
2025-07-10 08:48:22 +1000

    Turn off warnings.

--- process.sh
+++ process.sh
@@ -7,6 +7,8 @@ export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
 export NUMEXPR_NUM_THREADS=1
+export OMP_DISPLAY_ENV=FALSE
+export KMP_WARNINGS=FALSE
 
 for item in "."/*; do
     # Check if the current item is a regular file
